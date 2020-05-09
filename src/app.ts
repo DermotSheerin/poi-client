a735c7643df1a9d309a0df032c4e2cb0db35973c
Dermot Sheerin <d.sheerin@hotmail.com>
2020-05-09 11:17:17 +0100

    nav-bar added to project

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,26 @@
-export class App {
+import { RouterConfiguration, Router } from 'aurelia-router';
+import { PLATFORM } from 'aurelia-pal';
 
+export class App {
+  router: Router;
+
+  configureRouter(config: RouterConfiguration, router: Router) {
+    config.map([
+      {
+        route: ['', 'islands'],
+        name: 'Island',
+        moduleId: PLATFORM.moduleName('views/islands'),
+        nav: true,
+        title: 'Add Islands'
+      },
+      // {
+      //   route: 'candidates',
+      //   name: 'candidates',
+      //   moduleId: PLATFORM.moduleName('views/candidates'),
+      //   nav: true,
+      //   title: 'Candidate'
+      // }
+    ]);
+    this.router = router;
+  }
 }
