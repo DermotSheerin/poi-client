ceda01889a0601a2a73b959a90a181cb66ff526a
Dermot Sheerin <d.sheerin@hotmail.com>
2020-05-16 10:33:32 +0100

    First implementation of Region selection implemented and working

--- src/services/island-types.ts
+++ src/services/island-types.ts
@@ -1,14 +1,13 @@
 export interface Island {
-  // region: string;
-  region:string;
+  //region:string;
   name: string,
   description: string,
   latitude: number,
   longitude: number,
 }
 
-export interface Region {
-  region: string;
-}
+// export interface Region {
+//   region: string;
+// }
 
 // Put REGION in its own class??
