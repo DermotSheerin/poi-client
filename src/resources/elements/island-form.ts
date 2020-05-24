974ac3c245ae4f5082b2d97967ddcc574411723c
Dermot Sheerin <d.sheerin@hotmail.com>
2020-05-24 01:26:23 +0100

    Category dropdown view complete for add Island and view islands

--- src/resources/elements/island-form.ts
+++ src/resources/elements/island-form.ts
@@ -1,10 +1,9 @@
 import {bindable, inject} from 'aurelia-framework';
-import { Island } from '../../services/island-types';
+import {Island, RegionCategory} from '../../services/island-types';
 import { IslandService } from '../../services/island-service';
 
 @inject(IslandService)
 export class IslandForm {
-  region: string;
   name: string;
   description: string;
   latitude: number;
@@ -12,14 +11,14 @@ export class IslandForm {
   @bindable
   islands: Island[];
   @bindable
-  regions: string[];
-  selectedRegion = '';
+  regionCategories: RegionCategory[];
+
+  selectedRegionCategory: RegionCategory = null;
 
   constructor(private ds: IslandService) {}
 
-  addIsland() { // using ds to access methods in IslandService class and pass details entered in island-form to the islands array
-    console.log(`here is selectedRegion --- ${this.selectedRegion}`)
-    this.ds.addIsland(this.selectedRegion, this.name, this.description, this.latitude, this.longitude);
+  addIsland() { // using ds to access methods in IslandService class and access the addIsland method
+    this.ds.addIsland(this.selectedRegionCategory, this.name, this.description, this.latitude, this.longitude);
   }
 
 }
--- src/views/islands.ts
+++ src/views/islands.ts
@@ -1,19 +1,18 @@
 import { inject } from 'aurelia-framework';
-import { Island } from "../services/island-types";
+import {Island, RegionCategory} from "../services/island-types";
 import { IslandService } from '../services/island-service';
 
 @inject(IslandService)
 export class Islands {
-  islands: Island[] = [];
-  regions: string[];
-  islandTotal = 0;
+  islands: Island[];
+  regionCategories: RegionCategory[]; // NOTE naming convention for this in islands view i.e., region-category
+  islandTotal = 0; // NOTE the naming convention for this in the islands view is NOT the same as above
 
   constructor(private ds: IslandService) {  // The islands array is established as a reference to the islands array defined in the IslandService class.
     this.islands = ds.islands; // sets islands array to the one defined in IslandService
-    this.regions = ds.regions;
+    this.regionCategories = ds.regionCategories;
     this.islandTotal = ds.islandTotal;
+    console.log(`i am in islands.ts view-model ${this.regionCategories}`);
   }
 
-  // PLACE REGION INTO ITS OWN CLASS
-//  regions: Region[] = [];
 }
