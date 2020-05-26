b4892c2fb35aaed76e6425c619922c7ee754ff01
Dermot Sheerin <d.sheerin@hotmail.com>
2020-05-26 09:06:46 +0100

    Implemented region/category filter

---
+++ src/resources/elements/category-form.ts
@@ -0,0 +1,26 @@
+import {bindable, inject} from 'aurelia-framework';
+import {Island, RegionCategory} from '../../services/island-types';
+import { IslandService } from '../../services/island-service';
+
+@inject(IslandService)
+export class CategoryForm {
+  @bindable
+  regionCategories: RegionCategory[];
+  @bindable
+  filterIslands: Island[];
+
+  // store region selected by the user on the form in selectedRegionCategory
+  selectedRegionCategory: RegionCategory = null;
+
+  constructor(private ds: IslandService) {
+    console.log(`here in Category-Form Constructor`)
+  }
+
+  addRegionCategory() { //
+    this.ds.addRegionCategory();
+  }
+
+  async categoryFilter() { // pass the selected region to the categoryFilter in IslandService class to retrieve the islands belonging to this region
+    let categoryFilter = await this.ds.categoryFilter(this.selectedRegionCategory);
+  }
+}
--- src/resources/elements/category-list.ts
+++ src/resources/elements/category-list.ts
@@ -1,33 +1,16 @@
 import {bindable, inject} from 'aurelia-framework';
-import { RegionCategory } from '../../services/island-types';
-import { IslandService } from '../../services/island-service';
+import {Island} from '../../services/island-types';
 
-@inject(IslandService)
+
 export class CategoryList {
   @bindable
-  regionCategories: RegionCategory[];
-  selectedRegionCategory: RegionCategory = null;
-  //filterSelection: RegionCategory = null;
-  filterIslands = [];
+  filterIslands: Island[];
 
-  constructor(private ds: IslandService) {
-    console.log(`here in RegionCategoryList ${this.selectedRegionCategory}`);
-  }
-
-  addRegionCategory() { //
-    this.ds.addRegionCategory();
-  }
-
-  async categoryFilter() {
-    const categoryFilter = await this.ds.categoryFilter(this.selectedRegionCategory);
-    //console.log(`last selRegCat ---> ${categoryFilter}`)
-    categoryFilter.forEach(island => this.filterIslands.push(island));
-   // this.filterIslands.push(categoryFilter);
-  }
-
-  // categoryFilter() { //
-  //   const categoryFilter =  this.ds.categoryFilter(this.selectedRegionCategory);
-  //   this.filterIslands.push(categoryFilter);
+  // async categoryFilter() {
+  //   this.filterIslands = []; // clear the filterIslands array before each filter request
+  //   let categoryFilter = await this.ds.categoryFilter(this.selectedRegionCategory);
+  //   categoryFilter.forEach(island => this.filterIslands.push(island));
   // }
+
 
 }
