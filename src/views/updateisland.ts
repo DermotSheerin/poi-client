d24a3db4d0b57124a973e16829fe395bde0dfa39
Dermot Sheerin <d.sheerin@hotmail.com>
2020-06-07 22:16:18 +0100

    validation and error catching

--- src/views/updateisland.ts
+++ src/views/updateisland.ts
@@ -1,12 +1,15 @@
 import {inject} from 'aurelia-framework';
 import { IslandService } from '../services/island-service';
 import { ImageStore } from '../services/image-store';
+import {RegionCategory} from "../services/island-types";
 
 @inject(IslandService, ImageStore)
 export class UpdateIsland {
-    currentIslandDetails;
+  currentIslandDetails;
+  regionCategories: RegionCategory[];
 
   constructor(private ds: IslandService, private is: ImageStore) {
+    this.regionCategories = this.ds.regionCategories;
   }
 
   // https://aurelia.io/docs/api/router/interface/RoutableComponentActivate/method/activate/ & https://elanderson.net/2015/10/aurelia-routing-with-a-parameter/
